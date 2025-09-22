1bbad5f158e363ae2a7055c2fdd1f9436aac5498
Chandan07Gowda <chandannallurkrishnappa@gmail.com>
2025-09-23 01:42:32 +0530

    Fix security hotspots (batch of 2 issues)
    - backend/routes/user.js: S5689
    - backend/routes/taskRoutes.js: S5689

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,9 @@
-const express=require('express');
-const User=require('../controller/user')
-const router=express();
+const express = require('express');
+const User = require('../controller/user');
+const router = express.Router(); // Use Router() from express to create a new instance of a middleware chain for the given route prefix ("/").
 
+// Fix 1: Use .post instead of .route('/path').post
+router.post('/user/register', User.register);
+router.post('/user/login', User.login);
 
-router.route('/user/register').post(User.register);
-
-router.route('/user/login').post(User.login);
-
-module.exports=router;
+module.exports = router; // Export the router instance to be used by the app.
