a9c6a235a16d441ce4c34e94770389469178e6f8
Chandan07Gowda <chandannallurkrishnappa@gmail.com>
2025-10-20 14:00:04 +0530

    Fix security hotspots (batch of 3 issues)
    - backend/routes/taskRoutes.js: S5689
    - backend/app.js: S5689
    - backend/routes/user.js: S5689

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const User = require('../controller/user');
-const router = express.Router(); // Use Router() instead of creating a new instance of express
+
+const router = express.Router();
 
 // Fix 1: Use Router() to create a separate instance for each route
 router.post('/user/register', User.register);
 router.post('/user/login', User.login);
 
-module.exports = router; // Export the router instance
+module.exports = router;
